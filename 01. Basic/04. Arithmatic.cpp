b75531436620f51f94cff2c23e4761a4c6389cd3
Juwel-771 <juwelah99@gmail.com>
2023-03-03 19:28:44 +0600

    Arithmetic Code Added

---
+++ 01. Basic/04. Arithmatic.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter first number: " <<endl;
+    cin >> num1;
+    cout << "Enter second number: " <<endl;
+    cin >>num2;
+
+    int sum = num1+num2;
+    cout <<sum <<endl;
+
+    int sub = num1-num2;
+    cout <<sub <<endl;
+
+    int div = num1/num2;
+    cout <<div <<endl;
+
+    int multi = num1*num2;
+    cout <<multi <<endl;
+
+    int remain = num1%num2;
+    cout <<remain;
+
+}
