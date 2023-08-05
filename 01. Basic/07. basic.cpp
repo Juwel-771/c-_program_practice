ca2d81dc1610b8eaccb3107c57de24241f928732
Juwel-771 <juwelah99@gmail.com>
2023-08-05 13:42:07 +0600

    Reverse

---
+++ 01. Basic/07. basic.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    char md[20] = "Md Juwel";
+    cout << md;
+}
---
+++ 01. Basic/takingInput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    char name[20];
+
+    cout << "Enter Your Name: ";
+    cin >> name;
+
+    cout << name;
+    getch();
+}
