bce9106fd7f0c2d1d0f99719447b51eacbc8b233
Juwel-771 <juwelah99@gmail.com>
2023-08-12 04:48:45 +0600

    if-statement

--- 01. Basic/evenOdd.cpp
+++ 01. Basic/evenOdd.cpp
@@ -3,16 +3,44 @@ using namespace std;
 
 int main()
 {
-    int num;
+    int num, num2;
 
     cout << "Enter Your Number: ";
-    cin >> num;
+    cin >>num >>num2;
 
     if(num > 0){
-        cout << "Positive Number";
+        cout << "Positive Number" <<endl;
     } else if(num < 0){
-        cout << "Negative Number";
+        cout << "Negative Number" <<endl;
     } else {
-        cout << "Not A Number";
+        cout << "Not A Number" <<endl;
     }
+
+    if(num%2==0){
+        cout << "Even Number" <<endl;
+    } else {
+        cout << "Odd Number"<<endl;
+    }
+
+    if(num > num2){
+        cout << "Number 1 is Larger" <<endl;
+    } else if(num < num2){
+        cout << "Number 2 is Larger" <<endl;
+    } else{
+        cout << "Both are equal" <<endl;
+    }
+
+    int mark;
+
+    cout << "Enter your marks: ";
+    cin >> mark;
+
+    if(mark > 33){
+        cout << "Passed" <<endl;
+    } else {
+        cout << "failed" <<endl;
+    }
+
+
+    return 0;
 }
